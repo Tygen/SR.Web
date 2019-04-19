64c41c01276294b0ca95f92b16b41a39737cc9a0
Tiancheng <tygensun@gmail.com>
2019-04-19 20:57:11 +0800

    uploader working

--- client/templates/picture_cards/card_list.js
+++ client/templates/picture_cards/card_list.js
@@ -3,3 +3,7 @@ Template.cardList.helpers({
       return Pictures.find();    //return all data stored in collection "Pictures"
     }
   });
+
+  document.getElementById("file_input_file").onpropertychange = function () {
+    document.getElementById("fileuploadurl").value = this.value.replace(/C:\\fakepath\\/i, '');
+};
