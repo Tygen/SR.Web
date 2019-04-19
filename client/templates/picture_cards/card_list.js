02f3a25d1d1e42dfd879ea9916131097d8493e81
Tiancheng <tygensun@gmail.com>
2019-04-20 01:32:38 +0800

    uploader part finished and working!

--- client/templates/picture_cards/card_list.js
+++ client/templates/picture_cards/card_list.js
@@ -3,7 +3,3 @@ Template.cardList.helpers({
       return Pictures.find();    //return all data stored in collection "Pictures"
     }
   });
-
-  document.getElementById("file_input_file").onpropertychange = function () {
-    document.getElementById("fileuploadurl").value = this.value.replace(/C:\\fakepath\\/i, '');
-};
